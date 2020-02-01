b229741582b0f16526596d48464469c70ab44c72
MaxmdLira <maxwell.lira@poggers.com.br>
2020-02-01 01:18:57 -0300

    initial commit

---
+++ src/main/java/br/com/ajocar/Ajocar/AjocarApplication.java
@@ -0,0 +1,13 @@
+package br.com.ajocar.Ajocar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjocarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjocarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ajocar/Ajocar/AjocarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ajocar.Ajocar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjocarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
