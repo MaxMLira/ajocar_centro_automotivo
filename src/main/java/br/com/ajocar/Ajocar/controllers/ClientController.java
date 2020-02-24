1614decde694a92142f7e3b3932bef0df6891a13
MaxMLira <maxwell.lira@poggers.com.br>
2020-02-24 11:56:42 -0300

    creating first method list clients

--- src/main/java/br/com/ajocar/Ajocar/controllers/ClientController.java
+++ src/main/java/br/com/ajocar/Ajocar/controllers/ClientController.java
@@ -1,20 +1,30 @@
 package br.com.ajocar.Ajocar.controllers;
 
+import br.com.ajocar.Ajocar.model.Client;
+import br.com.ajocar.Ajocar.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/client")
 public class ClientController {
-		@GetMapping("/")
-		public ModelAndView index() {
 
-			ModelAndView home = new ModelAndView();
+    @Autowired
+    private ClientService service;
 
-			home.setViewName("client");
-			return home;
-		}
+
+    @GetMapping("")
+	public ModelAndView index() {
+		ModelAndView home = new ModelAndView();
+        List<Client> clients = service.getAll();
+		home.setViewName("client");
+        home.addObject("clients",clients);
+		return home;
 	}
+}
 
--- src/main/java/br/com/ajocar/Ajocar/services/ClientService.java
+++ src/main/java/br/com/ajocar/Ajocar/services/ClientService.java
@@ -1,4 +1,23 @@
 package br.com.ajocar.Ajocar.services;
 
+
+import br.com.ajocar.Ajocar.model.Client;
+import br.com.ajocar.Ajocar.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+
+
+    public List<Client> getAll(){
+        return repository.findAll();
+    }
+
+
 }
