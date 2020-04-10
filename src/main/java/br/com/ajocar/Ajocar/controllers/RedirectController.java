2cfcd4afe61344885f564b999ae28762e9ddd99e
MaxMLira <maxwell.lira@poggers.com.br>
2020-04-10 20:00:07 -0300

    first redirect controller

---
+++ src/main/java/br/com/ajocar/Ajocar/controllers/RedirectController.java
@@ -0,0 +1,25 @@
+package br.com.ajocar.Ajocar.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/redirectPage")
+public class RedirectController {
+
+    @GetMapping("/{idClient}")
+    public ModelAndView redirectToOrderPage(@PathVariable Integer idClient){
+
+        ModelAndView redirect = new ModelAndView();
+
+        redirect.setViewName("redirect");
+        redirect.addObject("client",idClient);
+        return redirect;
+
+    }
+
+}
