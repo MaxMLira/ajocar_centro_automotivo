1a8fd85fed73d6943170abe250bc36fb2ec182e5
MaxMLira <maxwell.lira@poggers.com.br>
2020-11-17 22:36:04 -0300

    Removing mock and imports

--- src/main/java/br/com/ajocar/Ajocar/AjocarApplication.java
+++ src/main/java/br/com/ajocar/Ajocar/AjocarApplication.java
@@ -1,24 +1,12 @@
 package br.com.ajocar.Ajocar;
 
-import br.com.ajocar.Ajocar.repositories.CarRepository;
-import br.com.ajocar.Ajocar.repositories.ClientRepository;
-import br.com.ajocar.Ajocar.repositories.ProductRepository;
-import br.com.ajocar.Ajocar.repositories.ServiceOrderRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AjocarApplication{
 
-	@Autowired
-	ClientRepository clientRepo;
-	@Autowired
-	CarRepository carRepo;
-	@Autowired
-	ServiceOrderRepository serviceRepo;
-	@Autowired
-	ProductRepository productRepo;
+
 	
 	public static void main(String[] args) {
 		SpringApplication.run(AjocarApplication.class, args);
